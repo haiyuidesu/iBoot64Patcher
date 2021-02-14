df991f507821f1318dc0ee6dda711db127c50004
Yui Aioi <40716335+haiyuidesu@users.noreply.github.com>
2021-02-14 11:26:34 +0100

    Go command 'patch'
    This is a test, it may be removed later...

--- iBoot64Patcher.c
+++ iBoot64Patcher.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-uint64_t base = 0, version = 0, paced = 0;
+uint64_t base = 0, version = 0, paced = 0, extra = 0;
 
 #define bswap32(x) __builtin_bswap32(x)
 
@@ -18,29 +18,58 @@ uint64_t xref64(const uint8_t *ibot, uint64_t start, uint64_t end, uint64_t what
 
   memset(value, 0, sizeof(value));
 
-  end &= ~3;
+  end &= ~0x3;
 
-  for (i = start & ~3; i < end; i += 4) {
-    uint32_t op = * (uint32_t * )(ibot + i);
+  for (i = start & ~0x3; i < end; i += 0x4) {
+    uint32_t op = *(uint32_t *)(ibot + i);
     unsigned reg = op & 0x1f;
 
-    // iOS 8+
-    if ((op & 0x9f000000) == 0x10000000) {
-      signed adr = ((op & 0x60000000) >> 18) | ((op & 0xffffe0) << 8);
-      value[reg] = ((long long) adr >> 11) + i;
+    if ((op & 0x9f000000) == 0x90000000) {
+      signed adr = ((op & 0x60000000) >> 0x12) | ((op & 0xffffe0) << 8);
+
+      value[reg] = ((long long) adr << 1) + (i & ~0xfff);
     } else if ((op & 0xff000000) == 0x91000000) {
-      unsigned rn = (op >> 5) & 0x1f;
-      unsigned shift = (op >> 22) & 3;
-      unsigned imm = (op >> 10) & 0xfff;
+      unsigned rn = (op >> 0x5) & 0x1f;
+
+      if (rn == 0x1f) {
+        value[reg] = 0;
+        continue;
+      }
+
+      unsigned shift = (op >> 0x16) & 0x3;
+      unsigned imm = (op >> 0xA) & 0xfff;
 
       if (shift == 1) {
-        imm <<= 12;
+        imm <<= 0xC;
       } else {
         if (shift > 1) continue;
       }
 
       value[reg] = value[rn] + imm;
-    } // iOS 7
+    } else if ((op & 0xf9C00000) == 0xf9400000) {
+      unsigned rn = (op >> 0x5) & 0x1f;
+      unsigned imm = ((op >> 0xA) & 0xfff) << 0x3;
+
+      if (!imm) continue;
+
+      value[reg] = value[rn] + imm;
+    } else if ((op & 0x9f000000) == 0x10000000) {
+      signed adr = ((op & 0x60000000) >> 0x12) | ((op & 0xffffe0) << 8);
+
+      value[reg] = ((long long)adr >> 0xB) + i;
+    } else if ((op & 0xff000000) == 0x58000000) {
+      unsigned adr = (op & 0xffffe0) >> 3;
+
+      value[reg] = adr + i;
+    } else if ((op & 0xfc000000) == 0x94000000) {
+      signed imm = (op & 0x3ffffff) << 2;
+
+      if (op & 0x2000000) imm |= 0xf << 0x1c;
+
+      unsigned adr = (unsigned)(i + imm);
+
+      if (adr == what) return i;
+    }
 
     if (value[reg] == what && reg != 0x1f) return i;
   }
@@ -178,6 +207,46 @@ uint64_t enable_kernel_debug(void *ibot, unsigned int length) {
   return 0;
 }
 
+// This 'patch' is for the 'go' command, nothing amazing at all
+uint64_t allow_any_imagetype(void *ibot, unsigned int length) {
+  printf("\n[%s]: allowing any type of images ('go' command)...\n", __func__);
+
+  uint64_t insn = 0, addr = 0;
+
+  void *bl = memmem(ibot, length, "Memory image not valid", strlen("Memory image not valid"));
+
+  if (bl == NULL) return -1;
+
+  uint64_t xref = xref64(ibot, 0x0, length, bl - ibot);
+
+  if (xref == 0) return -1;
+
+  insn_set(addr, 0x20, 0x2C, 0x2C, 0x30, hex_set(6723, 0x30, 0x34), 0x30);
+
+  insn = ((version < 3406) ? 0x040080d2 : 0x050080d2); // mov x4, #0 | mov x5, #0
+
+  *(uint32_t *)(ibot + xref - addr) = bswap32(insn);
+
+  // iOS 7 use a second time the x4 register so let's clear this !
+  if (version == 1940) *(uint32_t *)(ibot + xref - addr + 0x4) = bswap32(insn); 
+
+  printf("[%s]: patched to MOV %s, #0 insn = 0x%llx\n",
+    __func__, ((version < 3406) ? "x4" : "x5"), (xref + base) - addr);
+
+  insn_set(addr, 0x14, 0x10, 0x24, 0x28, 0xC, 0x28);
+
+  insn = ((version < 3406) ? 0x05008052 : 0x050080d2); // mov w5, #0 | mov w6, #0
+
+  *(uint32_t *)(ibot + xref - addr) = bswap32(insn);
+
+  printf("[%s]: patched to MOV %s, #0 insn = 0x%llx\n",
+    __func__, ((version < 3406) ? "w5" : "w6"), (xref + base) - addr);
+
+  printf("[%s]: successfully allowed any image types ('go' command)!\n", __func__);
+
+  return 0;
+}
+
 uint64_t apply_generic_patches(void *ibot, unsigned int length) {
   // Looking if the iBoot has a kernel load routine
   if (memmem(ibot, length, "__PAGEZERO", strlen("__PAGEZERO"))) {
@@ -195,14 +264,22 @@ uint64_t apply_generic_patches(void *ibot, unsigned int length) {
     return -1;
   }
 
+  if (extra) {
+    if (allow_any_imagetype(ibot, length) != 0) {
+      printf("[%s]: unable to allow any types of images ('go' command).\n", __func__);
+      return -1;
+    }
+  }
+
   return 0;
 }
 
 void usage(char *owo[]) {
   char *ibot = NULL;
   ibot = strrchr(owo[0], '/');
-  printf("usage: %s [-p] <in> <out>\n", (ibot ? ibot + 1 : owo[0]));
+  printf("usage: %s [-p] <in> <out> [-e]\n", (ibot ? ibot + 1 : owo[0]));
   printf("\t-p, --patch\tapply the generics patches.\n");
+  printf("\t-e, --extra\tapply extra patches (test only).\n");
   exit(1);
 }
 
@@ -211,11 +288,23 @@ int main(int argc, char *argv[]) {
   void *ibot = NULL;
   unsigned int length = 0, patch = 0;
 
-  if (argc != 4) goto usagend;
+  if (argc < 4) goto usagend;
 
   for (int i = 1; i < argc; i++) {
     if (!strcmp(argv[i], "-p") || !strcmp(argv[i], "--patch")) {
       patch = 1;
+
+      if (argv[i+3] != NULL) {
+        if (!strcmp(argv[i+3], "-e") || !strcmp(argv[i+3], "--extra")) {
+          extra = 1;
+
+          break;
+        } else {
+         printf("warning: unrecognized argument: %s\n", argv[i+3]);
+          goto usagend;
+        }
+      }
+
       break;
     } else {
       printf("warning: unrecognized argument: %s\n", argv[i]);
@@ -224,6 +313,8 @@ int main(int argc, char *argv[]) {
   }
 
   if (patch) {
+    printf("[%s]: starting...\n", __func__);
+
     fd = fopen(argv[2], "rb");
 
     if (!fd) {
